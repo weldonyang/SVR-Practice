2174ee024825db9824e2c75abb42678eb4006850
weldonyang <51804539+weldonyang@users.noreply.github.com>
2021-01-12 19:51:25 -0800

    First commit

---
+++ support_vector_regression.py
@@ -0,0 +1,18 @@
+# Support Vector Regression (SVR)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+# Feature Scaling
+
+# Training the SVR model on the whole dataset
+
+# Predicting a new result
+
+# Visualising the SVR results
+
+# Visualising the SVR results (for higher resolution and smoother curve)
